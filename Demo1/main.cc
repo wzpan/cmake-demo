4066a8d33694b29d332c6392f3085790780726eb
潘伟洲 <panweizhou500@pingan.com.cn>
2016-07-22 19:04:00 +0800

    fix a bug when exponent is 0.

--- Demo1/main.cc
+++ Demo1/main.cc
@@ -12,6 +12,10 @@ double power(double base, int exponent)
 {
     int result = base;
     int i;
+
+    if (exponent == 0) {
+        return 1;
+    }
     
     for(i = 1; i < exponent; ++i){
         result = result * base;
